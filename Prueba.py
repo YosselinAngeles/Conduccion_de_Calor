b283ab93603bdd207abd590112696cf391aeee7e
oscarteran <oscar-hdzteran@hotmail.com>
2020-10-22 20:07:14 -0500

    Modificado Jueves 22

--- Prueba.py
+++ Prueba.py
@@ -1,13 +1,36 @@
-# Archivo para la lectura de datos
+import numpy as np
 
-#Lectura de datos
+print()
+print("+----------------------------------------------------+")
+print("|      Solucion de la transferencia de calor         |")
+print("+----------------------------------------------------+")
+print("| Autores: Hernández Terán Oscar                     |")
+print("|          Ángeles Yosselin                          |")
+print("|          García Andrés                             |")
+print("+----------------------------------------------------+")
+print("|                 Datos de entrada                   |")
+print("+----------------------------------------------------+")
 
 
-
-a = float(input("Ingrese el comienzo de la barra.       a="))
-b = float(input("Ingrese el fin de la barra.            b="))
+a = float(input("Ingrese el comienzo de la barra.                a="))
+b = float(input("Ingrese el fin de la barra.                     b="))
 K = float(input("Ingresa la conductividad térmica del material   k="))
-Nodos = int(input("Ingresa el número de nodos que desea       N="))
-Ta = float(input("Ingrese la temperaruta al inicio.        Ta="))
-Tb = float(input("Ingrese la temperaruta al final.         Tb="))
+Nodos = int(input("Ingresa el número de nodos que desea            N="))
+Ta = float(input("Ingrese la temperaruta al inicio.               Ta="))
+Tb = float(input("Ingrese la temperaruta al final.                Tb="))
 
+h = (b-a)/Nodos
+r = K/(h**2)
+
+def Arreglos(Nodos,Ta,Tb):
+    A = np.zeros((Nodos,Nodos))
+    T = np.zeros(Nodos)    
+    b = np.zeros(Nodos)
+    b[0] = Ta
+    b[-1] = Tb
+    return A, T, b
+
+A, T, b = Arreglos(Nodos,Ta,Tb)
+print(A)
+print(T)
+print(b)
