037b2f2bd0ee75cc24be355e9b9b82dd4dc64eb5
oscarteran <oscar-hdzteran@hotmail.com>
2020-10-22 19:32:03 -0500

    Primer script

---
+++ Prueba.py
@@ -0,0 +1,13 @@
+# Archivo para la lectura de datos
+
+#Lectura de datos
+
+
+
+a = float(input("Ingrese el comienzo de la barra.       a="))
+b = float(input("Ingrese el fin de la barra.            b="))
+K = float(input("Ingresa la conductividad térmica del material   k="))
+Nodos = int(input("Ingresa el número de nodos que desea       N="))
+Ta = float(input("Ingrese la temperaruta al inicio.        Ta="))
+Tb = float(input("Ingrese la temperaruta al final.         Tb="))
+
