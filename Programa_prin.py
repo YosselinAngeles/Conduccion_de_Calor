cbfc77dd46ca3b2d1e743f088ef4d9393816e4c2
YosselinAngeles <yosselin.angeles@hotmail.com>
2020-10-27 21:52:08 -0600

    Programa principal 21:50:00 27 oct

---
+++ Programa_prin.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 21:13:43 2020
+
+@author: yosselin
+"""
+from Prueba import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Programa principal 
+
+
+# Llamar a la función solución del sistema
+u = sol_sistema(A, b)
+
+# Ingresando las condiciones de frontera
+u[0] = Ta
+u[-1] = Tb
+
+# Impresion de los vectores y matrices
+print("\n Matriz del sistema : \n", A)
+print("\n Lado derecho del sistema : \n", b)
+print("\n Vector solucion:")
+print("El vectro solución es:")
+for i in range(len(u)):
+    print(u[i])
+
+
+#Llamada de la función para Gráficar
+gradica=grafica_solucion(x, u)
+
