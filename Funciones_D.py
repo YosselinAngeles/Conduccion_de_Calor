0297bf0afcfd6485e4f0d47c58cf1f4494828e5b
oscarteran <oscar-hdzteran@hotmail.com>
2020-10-29 17:14:56 -0600

    Ambas versiones

---
+++ Funciones_D.py
@@ -0,0 +1,146 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ------------------------------------------------------------
+# ------------------------------------------------------------
+# Funcion para la creacion de los arreglos
+def Vector_aux(N,Ta,Tb):
+    """
+    Esta funcion genera un vector auxiliar, y se asignan los valores
+    a la frontera.
+    
+    Parameters
+    ----------
+    N : Entero (int)
+        Numero de nodos.
+    Ta : Real (float)
+        Temperatura al inicio.
+    Tb : Real (float)
+        Temperatura al final.
+
+    Returns  
+    -------
+    Variables de salida:
+                - Arreglo b
+    """  
+    b = np.zeros(N)
+    b[0] = -Ta
+    b[-1] = -Tb
+    return b
+
+# -------------------------------------------------
+# -------------------------------------------------
+# Llenado de la matriz 
+def creacion_matriz(N):
+    """
+    Esta funcion crea una matriz cuadrada de tamaño N y
+    cambia los valores de la diagonal    
+    
+    Parameters
+    ----------
+    N : Entero (int)
+        Número de nodos.
+
+    Returns
+    -------
+    A : Real(float)
+        Matriz(N,N).
+
+    """
+    A = np.zeros((N, N))
+    diagonal=-2
+    A[0,0] = diagonal; A[0,1] = 1
+    for i in range(1,N-1):
+        A[i,i] = diagonal
+        A[i,i+1] = 1
+        A[i,i-1] = 1
+    A[N-1,N-2] = 1; A[N-1,N-1] = diagonal
+    return A
+# -------------------------------------------------
+# -------------------------------------------------
+# Vector solucion
+def vector_sol(N):
+    """
+    Esta funcion genera un vector de tamaño N+2 que contendrá 
+    las soluciones y sus condiciones de frontera
+                                                
+    Parameters
+    ----------
+    N : Entera (int)
+        Número de nodos.
+
+    Returns
+    -------
+    u : Real (float)
+        arreglo solución.
+
+    """
+    u = np.zeros(N+2)
+    return u
+# --------------------------------------------------
+# --------------------------------------------------
+# Solucion del sistema
+def sol_sistema(A,b,N):
+    """
+    Esta funcion resuelve la ecuacion matricial y guarda los valores
+    en el vector solucion
+    
+    Parameters
+    ----------
+    A : Real(float)
+        Matriz cuadrada de tamaño N, con la diagonal editada.
+    b : Real(float)
+        Vector auxiliar de tamaño N, con las condiciones en la frontera.
+
+    Returns
+    -------
+    u : Real(float)
+        Vector solución del problema.
+    """
+    u = vector_sol(N)
+    u[1:-1] = np.linalg.solve(A,b)
+    return u
+
+#---------------------------------------------------
+# Graficando las soluciones
+def grafica_solucion(x,u):
+    """
+    Esta función genera las gráficas de la solución del problema.
+    Parameters
+    ----------
+    x : Real(float)
+        Vector que corresponde a las distancias de la barra.
+    u : Real(float)
+        Vector solución del problema.
+
+    Returns
+    -------
+    None.
+
+    """
+    plt.plot(x,u,'C3-o')
+    plt.title("Solucion a la ecuacion de calor")
+    plt.xlabel("Distancia [m]")
+    plt.ylabel("Temperatura [C]")
+    plt.grid(linestyle='--',linewidth=0.8)
+    plt.show()
+    return()
+
+
+# ---------------------------------------------------
+# Funcion de escritura de los datos
+def escritura(largo,Ta,Tb,N):
+    f = open("Archivo.txt", "w", encoding="utf8")
+    f.write("--------------------------------------")
+    f.write("ARCHIVO GENERADO CON LAS SOLUCIONES DE LA ECUACION DE CALOR")
+    f.write("--------------------------------------\n")
+    f.write("Longitud de la barra: " + str(largo) + "\n")
+    f.write("Temperatura en los extremos: " + str(Ta) +  " y " + str(Tb) +  "\n")
+    f.write("El numero de nodos es: " +  str(N) + "\n")
+    f.write("Los resultados son los siguentes: \n")
+    f.close()
+
+
+
+
+
---
+++ Programa_prin_D.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import Funciones_D as fun
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Programa principal 
+print()
+print("+----------------------------------------------------+")
+print("|      Solucion de la transferencia de calor         |")
+print("+----------------------------------------------------+")
+
+
+# Datos de entrada
+a = float(input("Ingrese el comienzo de la barra.                a="))
+b = float(input("Ingrese el fin de la barra.                     b="))
+K = float(input("Ingresa la conductividad térmica del material   k="))
+N = int(input("Ingresa el número de nodos que desea            N="))
+Ta = float(input("Ingrese la temperaruta al inicio.               Ta="))
+Tb = float(input("Ingrese la temperaruta al final.                Tb="))
+
+# Calculo de constantes necesarias
+h = (b-a)/(N+1)
+r = K/(h**2)
+x = np.linspace(a,b,N+2)
+largo = b-a
+
+# Impresion de las constantes 
+print("\n-------------------------------------------------")
+print("El ancho de la malla es: ",h)
+print("La constante r es:       ",r)
+print("El largo de la barra es: ",largo)
+print("---------------------------------------------------\n")
+
+# Llamado a funcion para crear arreglos
+b = fun.Vector_aux(N,Ta,Tb)
+
+#Llamando a la función creación de matriz
+A = fun.creacion_matriz(N)
+
+# Llamar a la función solución del sistema
+u = fun.sol_sistema(A, b, N)
+
+# Ingresando las condiciones de frontera
+u[0] = Ta
+u[-1] = Tb
+
+# Impresion de los vectores y matrices
+print("\n Matriz del sistema : \n", A)
+print("\n Lado derecho del sistema : \n", b)
+print("\n Vector solucion:")
+print("El vectro solución es:")
+for i in range(len(u)):
+    print(u[i])
+
+# Llamada de la función para Gráficar
+grafica = fun.grafica_solucion(x, u)
+plt.savefig("Solucion.png")
+
+# Llamado a la funcion de escritura
+archivo = fun.escritura(largo,Ta,Tb,N)
