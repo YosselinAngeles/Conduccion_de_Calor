8f185b637bee30ddd566ab72e5bb81957d6926cf
YosselinAngeles <yosselin.angeles@hotmail.com>
2020-11-09 13:46:44 -0600

    Programa con todas las calibraciones

--- Programa_prin_D.py
+++ Programa_prin_D.py
@@ -1,5 +1,5 @@
 
-import Funciones as fun
+import Funciones_D as fun
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -24,7 +24,8 @@ h = (b-a)/(N+1)
 r = K/(h**2)
 x = np.linspace(a,b,N+2)
 largo = b-a
-
+#g=len(x)
+#print("Tamaño",g)
 # Impresion de las constantes 
 print("\n-------------------------------------------------")
 print("El ancho de la malla es: ",h)
@@ -43,8 +44,69 @@ A = fun.creacion_matriz(N)
 u = fun.sol_sistema(A, b, N)
 
 # Ingresando las condiciones de frontera
+
+# ---- Programa 1 -------
+# Condiciones de Dirichlet  con Ta y Tb en las fronteras
+# u[0]=Ta
+# u[n]=Tb
 u[0] = Ta
 u[-1] = Tb
+
+#Solución analitica para un medio estacionario sin fuente o sumidero q=0
+a1 = fun.sol_analitica(Ta,Tb,x,N,largo)
+
+
+# ---- Calibración ----- 
+
+
+# ---- Calibración 1 ----
+# Condiciones de Dirichlet
+# Segunda derivada de u(x) = -f^2 u(x) con x [0,1]
+A0 = fun.creacion_matriz_diagonal(N,-2) #Siatema matricial para ec. de Poisson 1D
+#f0 = np.zeros(N)   
+f0= x[1:N+1]**2       # Lado derecho (columana de 1 y 0)
+#f = h*h*np.exp(x[1:N+1])
+#f0 = fun.Vector_aux(N,Ta,Tb,-1*q)
+f0[-1] += 1
+f0[0] -= 1
+u0 =fun.sol_sistema(A,b,N)
+
+u0[0] = 1
+u0[-1] = 1
+#u_0= []
+#u_0= -(f0**2) *(u0[1:N+1])
+
+densidad= 2
+b1 = 1
+a0 =fun.sol_analitica_cali1(x,N,densidad,b1)
+
+#print('solución analítica u0',u0)
+
+
+# --- Calibración 2 -----
+# Condiciones de Neuman
+# du/dn = 0
+# u(1) = 3
+A1 = fun.creacion_matriz_diagonal(N,-2) #Siatema matricial para ec. de Poisson 1D
+f = np.zeros(N)   
+f = h*h*np.exp(x[1:N+1])         # Lado derecho (columana de 1 y 0)
+
+f[0] += h*0    # Neumman
+f[N-1] -= 3
+A1[0,0] = -1; A1[0,1] = 1; # Ajuste de la matriz debido a Neumman
+
+u1 = fun.sol_sistema(A1,f,N)
+
+u1[0]= -h*0 + u1[1] # Condicion de frontera de Neumman
+u1[-1] = 3 
+
+# Solución análitica
+a2=fun.sol_analitica_cali2(x,N)
+
+# Error entre la solución numerica y la exacta
+Error1 = np.sqrt(h) * np.linalg.norm(fun.sol_analitica_cali2(x,N) - u1)
+print(" Error calibración 2 = %12.10g " % Error1)
+
 
 # Impresion de los vectores y matrices
 print("\n Matriz del sistema : \n", A)
@@ -54,9 +116,7 @@ print("El vector solución es:")
 for i in range(len(u)):
     print(u[i])
 
-#Solución analitica para un medio estacionario sin fuente o sumidero q=0
-a1 = fun.sol_analitica(Ta,Tb,x,N,largo)
-print('solución analítica a1',a1)
+
 
 #Calcula el error con respecto a la solucion analitica
     
@@ -64,7 +124,8 @@ Error = np.sqrt(h) * np.linalg.norm(a1 - u)
 print(" Error = %12.10g " % Error)
 
 # Llamada de la función para Gráficar
-grafica = fun.grafica_solucion(x, u,a1,"solución de la ecuación de calor","solución numérica", "solución exacta","Solucion.png" )
-
+grafica = fun.grafica_solucion(x, u,a1,"Solución de la Ecuación de Calor","Solución numérica", "Solución exacta","Solucion.png" )
+grafica0 = fun.grafica_solucion(x,u0,a0,"Calibración 1: Condiciones de Dirichlet","Solución numérica", "Solución exacta","Solucion_cali1.png" )
+grafica1 = fun.grafica_solucion(x,u1,a2,"Calibración 2: Condiciones de Neumman","Solución numérica", "Solución exacta","Solucion_cali2.png" )
 # Llamado a la funcion de escritura
 archivo = fun.escritura(largo,Ta,Tb,N)
