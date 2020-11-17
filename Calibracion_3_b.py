07d7fbce75803ddae7a5b0bca4e43719c7a0762e
oscarteran <oscar-hdzteran@hotmail.com>
2020-11-16 20:14:43 -0600

    Actualizado

--- Calibracion_3_b.py
+++ Calibracion_3_b.py
@@ -5,7 +5,7 @@ Created on Sat Nov 14 19:24:22 2020
 
 @author: yosselin
 """
-
+"""
 import Funciones_3 as fun
 import numpy as np
 import random
@@ -69,3 +69,69 @@ print("Solución: ",u1)
 #Grafica de la solución exacta y analítica 
 grafica0 = fun.grafica_solucion(x,K,"K(x)","Solución numérica", "Conductividad","Solucion_cali3k_b.png" )
 grafica1 = fun.grafica_solucion(x,u1,"Calibración 3: Conductividad aleatoria","Solución numérica", "Solución Numerica","Solucion_cali3_b.png" )
+
+"""
+# PROGRAMA PARA LA RESOLUCIÓN DE LA ECUACIÓN DE CALOR SIN FUENTES O SUMIDEROS
+
+import Funciones_Final as fun
+import numpy as np
+import random
+
+# Programa principal
+print()
+print("+----------------------------------------------------+")
+print("|      Solucion de la transferencia de calor         |")
+print("+----------------------------------------------------+")
+
+print('Opciones para la ejecución: \n'
+	  '1.- Tomar datos de ejemplo de un archivo \n'
+      '2.- Ingresar los datos manualmente.')
+
+sel = int(input('Escoja una opción.\n'))
+
+a,b,N,Ta,Tb,k,S,f = fun.Ingreso(sel)
+
+# Cálculo de Constantes
+h,x,lar = fun.Constantes(a,b,N)
+print("\n-------------------------------------------------")
+print("El ancho de la malla es: ",h)
+print("El largo de la barra es: ",lar)
+print("---------------------------------------------------\n")
+
+# Creación de matriz diagonal
+
+
+K=np.zeros(N+2)
+for i in range(0,N+2):
+   K[i]=(random.random())
+l=len(K)
+
+A= fun.Matriz_Diago3(N,-1,h,K)
+
+B = np.zeros(N)   
+
+for i in range(1,N-1):
+    B[0]=(-1*(K[i-1]+K[i])/2.)
+    B[-1] =(-1*(K[i+1]+K[i])/2.)
+    
+# Solucion del sistema
+u = fun.Sol_Sitema(A,B,N,Ta,Tb)
+
+# Solución analítica del problema
+u_exa = fun.Sol_Analitica(a,b,Ta,Tb,N)
+
+error = fun.Error(u,u_exa,N)
+
+print("\n--------------------------------------------")
+print("El vector b es: ",B)
+print("La matriz A es: \n",A)
+print("La solución numérica es: \n",u)
+print("La solución analítica es: \n",u_exa)
+print("El error en la solución es: \n",error)
+print("\n--------------------------------------------\n")
+
+# Graficando la solucion
+fun.Graficas_Cali3(x,K,u)
+
+# Guardando los datos
+fun.Escritura(u,u_exa)
